737ca4cf9d46ada428467f604bfcefdf2ef10fb3
ammar-333 <ammarshaban5@gmail.com>
2026-02-12 17:03:14 +0300

    initial commit

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // //localhost:port/api/Regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: localhost:port/api/Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+        }
+
+        // GET: localhost:port/api/Regions/{id}
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetRegionById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+                return NotFound();
+
+            return Ok(region);
+        }
+    }
+}
---
+++ NZWalks.API/Migrations/20260211124943_Initial migration1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegiomImageUrl",
+                table: "Regions",
+                newName: "RegionImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegionImageUrl",
+                table: "Regions",
+                newName: "RegiomImageUrl");
+        }
+    }
+}
